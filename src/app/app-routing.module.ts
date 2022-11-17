e255255087d93d8ccc309e651a79a4630fa9a382
Artem Hatsko <72604580+artem-gck@users.noreply.github.com>
2022-11-17 09:54:56 +0300

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountsComponent } from './accounts/accounts.component';
+import { DocumentsComponent } from './documents/documents.component';
+import { MainComponent } from './main/main.component';
+import { TasksComponent } from './tasks/tasks.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'tasks', component: TasksComponent },
+  { path: 'documents', component: DocumentsComponent },
+  { path: 'accounts', component: AccountsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { DocumentsComponent } from './documents/documents.component';
+import { AccountsComponent } from './accounts/accounts.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideMenuComponent,
+    TasksComponent,
+    DocumentsComponent,
+    AccountsComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
